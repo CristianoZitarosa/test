b54349506e19a8b792369972245fd14329823514
CristianoZitarosa <36627397+CristianoZitarosa@users.noreply.github.com>
2018-11-08 13:46:22 +0100

    Updates

---
+++ precache-manifest.c0ad06eed71e99f9c874964c12bcb0ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5ad1e219c88e053e0b1e",
+    "url": "/test/static/js/runtime~main.5ad1e219.js"
+  },
+  {
+    "revision": "e168cd335f44b9d434d8",
+    "url": "/test/static/js/main.e168cd33.chunk.js"
+  },
+  {
+    "revision": "1a2fe23bbee4e72efb16",
+    "url": "/test/static/js/1.1a2fe23b.chunk.js"
+  },
+  {
+    "revision": "e168cd335f44b9d434d8",
+    "url": "/test/static/css/main.ac95c21b.chunk.css"
+  },
+  {
+    "revision": "5ae6f4e06acf8996edc8ed99eae49573",
+    "url": "/test/index.html"
+  }
+];
